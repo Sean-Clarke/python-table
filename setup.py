2558ebbbceab1a8321006f941d767aa28a5b925d
Sean A Clarke <sean.alexander.clarke@gmail.com>
2018-07-09 15:15:39 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='Table',
+    version='0.1d',
+    description='Simple Python Table Data Structure Class',
+    author='Sean Clarke',
+    url='https://github.com/Sean-Clarke/python-table',
+    packages=['table'],
+    )
